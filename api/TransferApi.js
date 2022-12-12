4c336157de97cf205c6839d47dd8a68edbbc0ec0
Aine Oliveira <tiagomiranda.dev@gmail.com>
2022-12-11 23:34:49 -0300

    Created transferApi

---
+++ api/TransferApi.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import BASE_URL from '../constants/api';
+
+export const getItineraries = async (eventId, authorization) => {
+  try {
+    var response = await axios({
+      url: BASE_URL + 'api/transfers/' + eventId + '/itineraries',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + authorization,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    alert(error.response.data.errors);
+    return null;
+  }
+};
+
+export const registerItineraryAccess = async (
+  authorization,
+  code,
+  itineraryId,
+) => {
+  try {
+    var payload = {code: code, itineraryId: itineraryId};
+    var response = await axios({
+      url: BASE_URL + 'api/transfers/accesses',
+      method: 'POST',
+      data: payload,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + authorization,
+      },
+    });
+    return true;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    alert(error.response.data.errors);
+    return null;
+  }
+};
