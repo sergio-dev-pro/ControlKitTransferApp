337c764daee73fc7e8eb106943fbc84eef7abbb7
Sergio <sergiolucasoliveira02@gmail.com>
2023-02-04 13:56:31 -0300

    save changes

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -149,26 +149,17 @@ export function AuthProvider({children}) {
       await AsyncStorage.setItem('userToken', token);
       // decode token to get events.
       var decodedToken = jwt_decode(token);
-      console.log('decodedToken='+ JSON.stringify(decodedToken));
+      console.log('decodedToken=' + JSON.stringify(decodedToken));
       const events = JSON.parse(decodedToken.Events);
       // Se tiver apenas um evento, nao precisa ir para tela de selecao.
       const selectedEventId = events.length === 1 ? events[0].id : null;
-      if (selectedEventId) {
-        try {
-          const {data: requiredFieldsForUserRegistration} =
-            await getEventRequiredFields(selectedEventId);
-            await AsyncStorage.setItem(
-              'event',
-              JSON.stringify({
-                id: selectedEventId.toString(),
-                requiredForms: getRequiredForms(requiredFieldsForUserRegistration),
-              }),
-            );
-        } catch (error) {
-          console.error(error);
-        }
 
-      }
+      const eventRequiredFields = selectedEventId
+        ? await getEventRequiredFields(selectedEventId)
+        : null;
+      const requiredForms = eventRequiredFields
+        ? getRequiredForms(eventRequiredFields.data)
+        : null;
       let authStateChanges = {
         userToken: token,
         isAuthenticated: true,
@@ -176,6 +167,20 @@ export function AuthProvider({children}) {
         selectedEventId,
         canCreateTicket: decodedToken.CanCreateTicket,
       };
+      if (requiredForms) {
+        authStateChanges.requiredForms = requiredForms;
+        try {
+          await AsyncStorage.setItem(
+            'event',
+            JSON.stringify({
+              id: selectedEventId.toString(),
+              requiredForms,
+            }),
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      }
       setAuthState(prevState => ({
         ...prevState,
         ...authStateChanges,
