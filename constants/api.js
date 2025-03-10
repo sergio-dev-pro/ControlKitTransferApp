7552246c05bffd3ebc0ccc068f9f6a5298a2fc75
tiagomdev <34274233+tiagomdev@users.noreply.github.com>
2025-03-10 16:36:01 -0300

    updated api.js

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,5 @@
 const BASE_URL = 'https://meetingpoint-api.credenciamentospr.com/v1';
 
-export default BASE_URL
+const BASE_URL_V2 = 'https://tickets-meetingpoint-api-dev.credenciamentospr.com/v1';
+
+export { BASE_URL, BASE_URL_V2 };
