41c7107537b9ea26db99c1942812c149dbe805dc
tiagomdev <34274233+tiagomdev@users.noreply.github.com>
2025-07-05 03:15:54 -0300

    Created boardingApi

---
+++ api/BoardingApi.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+import BASE_URL_V2 from '../constants/api2';
+
+
+export const startBoarding = async (userToken, eventId, itineraryId, vehicleId) =>
+{
+  var data = {
+    itineraryId, vehicleId
+  };
+  try {
+    var response = await axios({
+        url: BASE_URL_V2 + `/events/${eventId}/Transports/Boardings`,
+        method: 'POST',
+        data,
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + userToken
+        },
+      });
+      console.log('@@@boardingCreated=' + JSON.stringify(response.data))
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    Alert.alert('', error.response.data.message);
+    return null;
+  }
+};
+
+export const finishBoarding = async (userToken, eventId, boardingId) =>
+{
+  try {
+    var response = await axios({
+        url: BASE_URL_V2 + `/events/${eventId}/Transports/Boardings/${boardingId}/Finish`,
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + userToken
+        },
+      });
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    Alert.alert('', error.response.data.message);
+    return null;
+  }
+};
+
+export const registerTicketBoarding = async (userToken, eventId, boardingId, accessKey) =>
+{
+  var data = {
+    accessKey
+  };
+  try {
+    await axios({
+        url: BASE_URL_V2 + `/events/${eventId}/Transports/Boardings/${boardingId}/Scan`,
+        method: 'PUT',
+        data,
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + userToken
+        },
+      });
+    return true;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    Alert.alert('', error.response.data.message);
+    return null;
+  }
+};
+
+
+
+export const getVehicles = async (userToken, eventId) =>
+{
+  try {
+    var response = await axios({
+        url: BASE_URL_V2 + `/events/${eventId}/Vehicles`,
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + userToken
+        },
+      });
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    Alert.alert('', error.response.data.message);
+    return null;
+  }
+};
+
+export const getItineraries = async (userToken, eventId) =>
+{
+  try {
+    var response = await axios({
+        url: BASE_URL_V2 + `/events/${eventId}/Itineraries`,
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + userToken
+        },
+      });
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    console.log('error error.response.data', error.response.data);
+    Alert.alert('', error.response.data.message);
+    return null;
+  }
+};
